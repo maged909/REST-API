09726d280d1e881f0d1cdd8dfd4dc7c749449a31
maged909 <mkingm123@gmail.com>
2022-02-25 22:43:11 +0200

    mongoose-pagination

--- models/post.js
+++ models/post.js
@@ -1,6 +1,10 @@
+const res = require('express/lib/response');
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
 
 const postSchema = new mongoose.Schema({
+    
     title:{
         type:String,
         required:true
@@ -11,4 +15,7 @@ const postSchema = new mongoose.Schema({
     },
 },{timestamps:true});
 
+postSchema.plugin(mongoosePaginate);
+
+const post = mongoose.model('Post', postSchema);
 module.exports=mongoose.model('Post',postSchema)
--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,32 @@
 require('dotenv').config();
-
 const express = require('express')
 const router = express.Router();
 const Post = require('../models/post')
-const User = require('../models/user')
-const paginatedResults = require('../middlewares/getPaginatedResults')
 const jwt = require('jsonwebtoken')
 const { JsonWebTokenError } = require('jsonwebtoken');
-// const authenticateToken = require('../middlewares/authenticateToken')
 const getOne = require('../middlewares/getOne');
 const { findById } = require('../models/user');
 
-router.get('/',authenticateToken,paginatedResults(Post), async(req,res)=>{
-    // res.send('Request has been sent');
-    try{
-        res.json({currentUser:res.user,page:res.paginatedResults})
-    }catch (err) {
-        res.status(500).json({message:err.message})
+// router.get('/',authenticateToken,(req,res)=>{
+//     res.redirect('/1-2');
+// })
+
+router.get('/',authenticateToken, (req,res)=>{
+    try {
+        var page=req.params.page || 1;
+        var limit=req.params.limit || 2;
+        Post.paginate({}, {page:page,limit: limit,collation: {locale:'en'}}, function(error, pageCount, paginatedResults) {
+            if (error) {
+              console.error(error);
+            } else {
+              res.json({'currentUser':res.user,'Pages': pageCount});
+            //   console.log(paginatedResults);
+            }
+        });
+    } catch (err) {
+        res.json({message:err.message})
     }
+    // res.send('Request has been sent');
 });
 router.get('/:id',getOne(Post), (req,res)=>{
     res.json(res.modelResult)
